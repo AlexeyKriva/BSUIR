4a6936266a965b2a73e8e57ea34d558c09c8d8fa
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-15 09:18:55 +0300

    Configs

---
+++ semester7/EYzEES/Lab1/SearchSystem/SearchServer/src/main/java/org/example/searchserver/configs/FlywayConfig.java
@@ -0,0 +1,24 @@
+package org.example.searchserver.configs;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class FlywayConfig {
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public Flyway flyway() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("db.migration")
+                .load();
+        flyway.migrate();
+
+        return flyway;
+    }
+}
---
+++ semester7/EYzEES/Lab1/SearchSystem/SearchServer/src/main/java/org/example/searchserver/configs/JpaConfig.java
@@ -0,0 +1,45 @@
+package org.example.searchserver.configs;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+@Configuration
+public class JpaConfig {
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    //@DependsOn("flyway")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibarnate.dialect", "org.hibarnate.dialect.PostgreSQLDialect");
+        jpaProperties.put("hibernate.hbm2ddl.auto", "update");
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(dataSource);
+        localContainerEntityManagerFactoryBean.setPackagesToScan("org.example.searchserver.entities");
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        localContainerEntityManagerFactoryBean.setJpaDialect(new HibernateJpaDialect());
+        localContainerEntityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return localContainerEntityManagerFactoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager(EntityManagerFactory managerFactory) {
+        return new JpaTransactionManager(managerFactory);
+    }
+}
